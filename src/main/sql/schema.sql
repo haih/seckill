b83df06c51bf13975072c566f16519a424fd856c
haih <huhai2005@126.com>
2016-05-25 22:14:30 +0800

    add spring configure files

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE seckill(
 	name VARCHAR(120) NOT NULL COMMENT 'production name',
 	number int NOT NULL COMMENT 'stock number',
 	start_time timestamp NOT NULL COMMENT 'seckill start_time',
-	-- end_time timestamp not null comment 'seckill end_time',
+	end_time timestamp not null comment 'seckill end_time',
 	create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
 	PRIMARY KEY (seckill_id),
     key idx_start_time(start_time),
@@ -28,14 +28,8 @@ values
 -- seckill succeed detail information
 -- user login and authentication related info
 
-create table success_killed(
-	seckill_id bitint NOT NULL comment '秒杀商品id',
-	user_phone bitint NOT NULL comment '用户手机号',
-	state tinyint NOT NULL DEFAULT -1 COMMENT 'status flag:-1: invalid, 0:succeed, 1:paid',
-	create_time timestamp NOT NULL ,
-	PRIMARY KEY(seckill_id, user_phone), /*联合主键*/
-	key idx_create_time(create_time)
-)ENGINE=innoDB DEFAULT CHARSET=utf8 comment='秒杀成功明细表';
+
+
 
 
 
